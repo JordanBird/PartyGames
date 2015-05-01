5234f3a53e6e52419acf2753b68ef79a11805160
Abengoshis <alexandersaye@live.co.uk>
2015-05-01 04:25:34 +0100

    Smart Camera
    + Added smart camera.
    + Orbits the room.
    + Automatically chooses MP based on hits per second.
    + Zooms in on chosen MP or out to whole room.
    + Shakes randomly.
    + Added hits per second to MPs.

--- PartyGame/Assets/Scripts/MP_Control.cs
+++ PartyGame/Assets/Scripts/MP_Control.cs
@@ -2,24 +2,36 @@
 using System.Collections;
 
 public class MP_Control : MonoBehaviour {
-
+	
 	public string Party;
 	public float Health = 100;
+	public float HitsPerSecond { get; private set; }
+	
 	// Use this for initialization
 	void Start () {
-
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		HitsPerSecond -= Time.deltaTime;
+		if (HitsPerSecond < 0)
+			HitsPerSecond = 0;
+		
 		if (Health <= 0)
-			Dead ();
-
+			Die ();
+		
 	}
-
-	void Dead(){
+	
+	void Die(){
 		Destroy (this.gameObject);
 		// Maybe put increment a total of what ever party just got killed.
-
+		
+	}
+	
+	public void Punch(float damage)
+	{
+		++HitsPerSecond;
+		Health -= Mathf.Abs(damage);
 	}
 }
--- PartyGame/Assets/Scripts/PunchedDamage.cs
+++ PartyGame/Assets/Scripts/PunchedDamage.cs
@@ -17,8 +17,8 @@ public class PunchedDamage : MonoBehaviour {
 
 	void OnCollisionEnter(Collision other){
 		if (other.transform.tag == "Fist" && other.gameObject.GetComponent<PunchForce>().Party != Party) {
-			Debug.Log (Party+other.gameObject.GetComponent<PunchForce>().Party);
-				Parent.GetComponent<MP_Control> ().Health -= 30;
+//			Debug.Log (Party+other.gameObject.GetComponent<PunchForce>().Party);
+				Parent.GetComponent<MP_Control> ().Punch(30);
 
 
 		}
